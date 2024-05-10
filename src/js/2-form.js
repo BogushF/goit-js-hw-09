085b0b4ee5d7b7b5fe30c5c34816ae61b7e6a7e0
BogushF <boga1602@gmail.com>
2024-05-10 14:34:26 +0200

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,4 +3,37 @@ const formData = {
   message: '',
 };
 const STORAGE_KEY = 'feedback-form-state';
-const feedbackForm = document.querySelector('.feedback-form');
+const form = document.querySelector('.feedback-form');
+const email = form.elements.email;
+const message = form.elements.message;
+
+const preData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+if (preData === null) {
+  email.value = '';
+  message.value = '';
+} else {
+  email.value = preData.email;
+  message.value = preData.message;
+}
+
+function toInput(event) {
+  formData.email = event.currentTarget.elements.email.value.trim();
+  formData.message = event.currentTarget.elements.message.value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function toSubmit(event) {
+  event.preventDefault();
+  if (email.value && message.value) {
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+  } else {
+    alert('Fill please all fields');
+  }
+}
+
+form.addEventListener('input', toInput);
+form.addEventListener('submit', toSubmit);
